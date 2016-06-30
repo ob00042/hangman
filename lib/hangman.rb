130fe02fe12bbc90970b4539545993c0d3958e57
ob00042 <ob00042@gmail.com>
2016-06-30 18:38:07 +0300

    Loads and cleans dictionary, finds word within limits

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,1 +1,21 @@
 puts "Hangman Initialized!"
+
+alphabet = File.readlines "5desk.txt"
+alphabet.each do |entry|
+  entry.delete!("\n")
+end
+
+def find_word(array)
+word = ""
+loop do
+  word = array.sample
+  break if (word.length > 4) && (word.length < 13)
+end
+word
+end
+
+word = find_word(alphabet)
+puts word
+
+
+
