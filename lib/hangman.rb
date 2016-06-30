b0dabcd4884acd3ffba3f44020644957f1407e37
ob00042 <ob00042@gmail.com>
2016-06-30 21:04:04 +0300

    Saves and loadsgit add .!

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,8 +1,18 @@
+require "yaml"
+
+class Hangman
+
 puts "Hangman Initialized!"
 
+attr_accessor :word, :display_array, :trials_array, :count
 
+def initialize
 
-def find_word
+  
+
+end
+
+def self.find_word
 alphabet = File.readlines "5desk.txt"
 alphabet.each do |entry|
   entry.delete!("\n")
@@ -19,7 +29,7 @@ end
 # word = find_word(alphabet)
 # puts word
 
-def display_array(word)
+def self.display_array(word)
   display_array = Array.new(word.length, "_")
 
 end
@@ -27,7 +37,7 @@ end
 # disp_array = display_array(word)
 # print disp_array
 
-def guess_letter
+def self.guess_letter
 
   puts "Give me a letter for your guess!"
   guess = gets.chomp
@@ -36,7 +46,7 @@ def guess_letter
 
 end
 
-def check(guess, word, display_array, trials_array)
+def self.check(guess, word, display_array, trials_array)
   
   i = 0
   match = false
@@ -53,7 +63,7 @@ def check(guess, word, display_array, trials_array)
 
 end
 
-def game_over(display_array)
+def self.game_over(display_array)
 
   unless display_array.any?{  |letter| letter = "_" }
   	puts "You won!"
@@ -62,18 +72,35 @@ def game_over(display_array)
 
 end
 
-def game
+
+def self.game
   
   puts "Let's play Hangman!"
   puts "You have 10 rounds to find the word"
-  word = find_word
-  display_array = display_array(word)
-  trials_array = []
-  count = 1
+  puts "Do you want to load a game? (y/n)"
+  load = gets.chomp
+  if load == "y"
+  	file = load_game
+  	word = file[0]
+  	count = file[1]
+  	display_array = file[2]
+  	trials_array = file[3]
+  else
+    word = find_word
+    display_array = display_array(word)
+    trials_array = []
+    count = 1
+  end
+  play(word, count, display_array, trials_array)
+
+end
+
+def self.play(word, count, display_array, trials_array)
   10.times do 
+  	save_game(word, count, display_array, trials_array)
   	print display_array
   	puts ""
-  	puts "This is round #{count}"
+  	puts "This is round #{count} of 10"
   	puts "Failed letters: #{trials_array}"
   	count += 1
   	guess = guess_letter
@@ -81,7 +108,29 @@ def game
   	game_over(display_array)
   end
   puts "You lost!"
+  puts "The secret work was: #{word}"
+end
+
 
 end
 
-game
+def save_game(word, count, display_array, trials_array)
+  
+  puts "Do you want to save the game? (y/n)"
+  answer = gets.chomp
+  if answer == "y"
+      File.open("saved_game/saved.yml", "w")do |file| 
+        file.write(YAML.dump([word, count, display_array, trials_array]))
+	  end
+  end
+
+end
+
+def load_game
+
+    file = YAML::load(File.read("saved_game/saved.yml"))
+
+end
+
+
+Hangman.game
